8726e62151dc444e67db3172bf083ae1e5e6b2ed
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-08-01 22:23:06 +0700

    argv

--- pig_latin.js
+++ pig_latin.js
@@ -1,11 +1,14 @@
 'use strict'
 
 let pigLatin = (word) => {
-  let firstLetter = word.charAt(0);
-  if(isPowel(firstLetter)){
-    return word
-  }else{
-    return `${word.substr(1)}${firstLetter}ay`;
+  let splitFirstLeter =  word.split('');
+  for (let i = 0; i < splitFirstLeter.length; i++) {
+    if(isPowel(splitFirstLeter[0])) {
+      return word;
+    }
+    if(isPowel(splitFirstLeter[i])) {
+      return `${word.substr(i)}${word.slice(0,i)}ay`;
+    }
   }
 }
 
@@ -24,3 +27,5 @@ let convert = (sentence) => {
 }
 
 // Your CLI code here
+let args = process.argv.splice(2,3)
+console.log(convert(args));
