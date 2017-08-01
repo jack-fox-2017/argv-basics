134f8fe5f019bdba53d3a6bbc9ababcf6ccd370c
Renataclara <renataclarak@gmail.com>
2017-08-01 13:20:44 +0700

    done with readline

--- pig_latin.js
+++ pig_latin.js
@@ -24,3 +24,28 @@ let convert = (sentence) => {
 }
 
 // Your CLI code here
+
+// console.log(process.argv);
+// process.argv.forEach((val,index,array) => {
+//   console.log(`${index}: '${val}'`);
+// });
+//
+
+
+//https://nodejs.org/api/readline.html
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Input sentence here: '
+});
+
+rl.prompt();
+
+rl.on('line', (input) => {
+  console.log(`Received: ${input}`);
+  console.log(`Pig latin word: ${pigLatin(input)}`);
+});
+
+rl.prompt();
