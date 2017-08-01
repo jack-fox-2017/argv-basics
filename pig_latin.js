3de7365ea98e5f512f9ca1791d8517f402c93235
Pragistyo <new.SOS.hope@outlook.com>
2017-08-01 21:35:23 +0700

    argvs-pragistyo

--- pig_latin.js
+++ pig_latin.js
@@ -23,4 +23,22 @@ let convert = (sentence) => {
   return result.join(" ")
 }
 
+// process.argv.forEach((val, index, array) =>{
+// console.log(`${index}`: '${val}');
+// });
+// console.log(convert('hello wolrd'));
+
+//INGAAAAAATTTTTT, process.agrv itu ARRAAYYYY, convert mintanya satu string
+
+var ogi = ['blabla','blabla','hello', 'world']
+//AMBIL: hello sama world, trus join pake " " biar jadi satu string
+var potong = ogi.slice(2).join(' ')
+// var oke = potong.join(' ')
+// console.log(convert(potong))
+
+
+// console.log(convert(process.argv[2]) + ' ' + convert(process.argv[3]));
+ console.log(convert(process.argv.slice(2).join(' ')));
+// console.log(convert('hello world'))
+
 // Your CLI code here
