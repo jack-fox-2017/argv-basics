68c648de23266c0b7f07c28e61cfc9fed0278190
name": null <email": null>
2017-08-01 13:41:39 +0700

    tama

--- pig_latin.js
+++ pig_latin.js
@@ -2,21 +2,24 @@
 
 let pigLatin = (word) => {
   let firstLetter = word.charAt(0);
-  if(isPowel(firstLetter)){
+  if (isPowel(firstLetter)) {
     return word
-  }else{
+  }
+  else {
     return `${word.substr(1)}${firstLetter}ay`;
   }
 }
 
 let isPowel = (char) => {
-  return (/^[aeiou]$/i).test(char);
+  return (/^[aeiou]$/i)
+    .test(char);
 }
 
 let convert = (sentence) => {
   let result = []
-  let words = sentence.trim().split(/\s+/g)
-  for (let i=0;i<words.length;i++) {
+  let words = sentence.trim()
+    .split(/\s+/g)
+  for (let i = 0; i < words.length; i++) {
     result[i] = pigLatin(words[i])
   }
 
@@ -24,3 +27,7 @@ let convert = (sentence) => {
 }
 
 // Your CLI code here
+let proc = process.argv.splice(2)
+  .join(' ')
+
+console.log(convert(proc));
